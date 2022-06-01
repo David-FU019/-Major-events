fad1e6b32e7e259d2b0e52658cb28ce2db81d891
David-FU019 <www.1227754540@qq.com>
2022-06-01 11:17:25 +0800

    首页完成

---
+++ assets/js/index.js
@@ -0,0 +1,35 @@
+$.ajax({
+
+    url: "my/userinfo",
+
+    success: function (res) {
+        console.log(res);
+        //判断获取是否成功
+        if (res.status == 0) {
+            //渲染头像 --看用户是否设置了头像
+            if (res.data.user_pic !== null) {
+
+                $('.img-avatar').show().attr('src', res.data.user_pic)
+                $('.text-avatar').hide();
+            } else {
+                $('.img-avatar').hide();
+                $('.text-avatar').show().html((res.data.nickname || res.data.username).charAt(0).toUpperCase())
+
+            }
+            //渲染欢迎文字
+            $('.welcome span').html(res.data.nickname || res.data.username)
+        }
+    }
+});
+
+$('.logout').on('click', function (index) {
+    layer.confirm('是否退出确定', function () {
+
+        localStorage.removeItem('token')
+        location.href = "/login.html"
+        layer.close(index)
+
+    })
+
+
+})
